2a889f9fb1adaf386986530f49640127453416cd
MCVladIsLove <manukyan228008@mail.ru>
2024-05-24 14:53:56 +0300

    Syntax highlighter added

---
+++ Assets/Scripts/SyntaxHighlighting/StyleDeterminer.cs
@@ -0,0 +1,29 @@
+﻿using Assets.Scripts.LuaLexing;
+
+namespace Assets.Scripts.SyntaxHighlighting
+{
+    public class StyleDeterminer
+    {
+        public string GetStyleName(TokenType tokenType)
+        {
+            switch (tokenType)
+            {
+                case TokenType.Identifier:
+                    return "Normal"; // todo: add functionality to determine func, class etc
+                case TokenType.Keyword:
+                    return "Keyword";
+                case TokenType.Number:
+                    return "Number";
+                case TokenType.Operator:
+                    return "Operator";
+                case TokenType.String:
+                    return "String";
+                case TokenType.Comment:
+                    return "Comment";
+                default:
+                    return "Normal";
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SyntaxHighlighting/SyntaxHighlighter.cs
@@ -0,0 +1,33 @@
+﻿using Assets.Scripts.LuaLexing;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Scripts.SyntaxHighlighting
+{
+    public class SyntaxHighlighter
+    {
+        private StringBuilder _styledTextBuilder = new StringBuilder();
+        private StyleDeterminer _styleDeterminer = new StyleDeterminer();
+
+        public string GetHighlightedText(string text, List<Token> tokenizedText)
+        {
+            _styledTextBuilder.Clear();
+
+            foreach (Token token in tokenizedText)
+            {
+                if (token.type == TokenType.Whitespace || token.type == TokenType.Punctuation)
+                    _styledTextBuilder.Append(token.text);
+                else
+                    _styledTextBuilder
+                        .Append("<style=")
+                        .Append(_styleDeterminer.GetStyleName(token.type))
+                        .Append(">")
+                        .Append(token.text)
+                        .Append("</style>");
+            }
+
+            return _styledTextBuilder.ToString();
+        }
+    }
+
+}
